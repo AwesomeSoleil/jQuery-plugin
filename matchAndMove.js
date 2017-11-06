2c0871d2be154428aebb7a7b7f301767586659e5
Dmitriy Startsev <startsevd@gmail.com>
2017-11-06 10:11:47 +0300

    initial commit

---
+++ matchAndMove.js
@@ -0,0 +1,50 @@
+/*
+*
+* Options:
+* - cities: [],
+* - moveUp: boolean
+*/
+
+(function($) {
+    "use strict";
+    $.fn.matchAndMove = function(options) {
+        const self = this;
+        let ops = $.extend({
+            cities: ["Saint Petersburg", "Moscow", "Prague"],
+            moveUp: true
+        }, options);
+        let cities = ops.cities.map(function(cityName) {
+            return cityName.toLowerCase();
+        });
+        const APIKey = "bf03de8120fbabf27fdd083f9d1fddc0";
+        const baseAPICall = "http://api.openweathermap.org/data/2.5/weather?q=";
+        const metricSystem = "units=metric";
+        const message = " , but who cares?";
+        let listItems = self.children();
+        for (let i = 0; i < cities.length; i ++) {
+            for (let j = 0; j < listItems.length; j ++) {
+                let listItem = listItems[j];
+                let match = listItem.innerText.toLowerCase().includes(cities[i]); // if includes() is not acceptable, switch to indexOf();
+                if(match) {
+                    let url = baseAPICall + cities[i] + "&" + metricSystem + "&&APPID=" + APIKey;
+                    $.getJSON(url, function(data) {
+                        let appendText = " " + data["main"]["temp"].toFixed(0) + " degrees in " + data["name"] + message;
+                        listItem.innerText += appendText;
+                    });
+                    $(listItem).click(function() {
+                        $(this).fadeOut(function() {
+                            if (ops.moveUp) {
+                                $(this).css("display", "list-item").prependTo(self);
+                            } else {
+                                $(this).css("display", "list-item").appendTo(self);
+                            }
+                        });
+                    });
+                    i = cities.length;
+                    break;
+                }
+            }
+        }
+        return self;
+    };
+}(jQuery));

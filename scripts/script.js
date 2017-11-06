054d0e28a1a7007c334a5af6e986c1dd11ee01bc
Dmitriy Startsev <startsevd@gmail.com>
2017-11-06 23:25:22 +0300

    refactored

--- scripts/script.js
+++ scripts/script.js
@@ -24,13 +24,23 @@ $(document).ready(function() {
         const metricSystem = "units=metric";
         const message = " , but who cares?";
         let listItems = self.children();
+/*         function getTemperature(city) {
+            const APIKey = "bf03de8120fbabf27fdd083f9d1fddc0";
+            const baseAPICall = "http://api.openweathermap.org/data/2.5/weather?q=";
+            const metricSystem = "units=metric";
+            let url = baseAPICall + city + "&" + metricSystem + "&&APPID=" + APIKey;
+            $.getJSON(url).done(function(data) {
+                temp = data["main"]["temp"].toFixed(0);
+            });
+        } */
         function animate(li) {
             $(li).click(function() {
                 $(this).fadeOut(function() {
+                    $(this).css("display", "list-item");
                     if (ops.moveUp) {
-                        $(this).css("display", "list-item").prependTo(self);
+                        $(this).prependTo(self);
                     } else {
-                        $(this).css("display", "list-item").appendTo(self);
+                        $(this).appendTo(self);
                     }
                 });
             });
@@ -38,8 +48,9 @@ $(document).ready(function() {
         for (let i = 0; i < cities.length; i ++) {
             for (let j = 0; j < listItems.length; j ++) {
                 let listItem = listItems[j];
-                let match = listItem.innerText.toLowerCase().includes(cities[i]); // if includes() is not acceptable, switch to indexOf();
+                let match = listItem.innerText.toLowerCase().includes(cities[i]);
                 if(match) {
+                    // getTemperature(cities[i]);
                     let url = baseAPICall + cities[i] + "&" + metricSystem + "&&APPID=" + APIKey;
                     $.getJSON(url, function(data) {
                         let appendText = " " + data["main"]["temp"].toFixed(0) + " degrees in " + data["name"] + message;

1570859208121537aebaa808645c553b817cc18d
Dmitriy Startsev <startsevd@gmail.com>
2017-11-08 19:38:18 +0300

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -5,34 +5,29 @@
 * - moveUp: boolean
 */
 $(document).ready(function() {
-    $("#list").matchAndMove({cities: ["Moscow", "Washington, DC", "Saint Petersburg", "Prague"], moveUp: false}).css("color", "blue");
+    $("#list").matchAndMove({cities: ["Moscow", "Washington, DC", "Saint Petersburg", "Prague"], moveUp: false}).css("color", "blue"); //chainable !
 });
 
 (function($) {
     "use strict";
+
     $.fn.matchAndMove = function(options) {
         const self = this;
         let ops = $.extend({
             cities: ["Saint Petersburg", "Moscow", "Prague"],
             moveUp: true
         }, options);
+
         let cities = ops.cities.map(function(cityName) {
             return cityName.toLowerCase();
         });
+
         const APIKey = "bf03de8120fbabf27fdd083f9d1fddc0";
         const baseAPICall = "http://api.openweathermap.org/data/2.5/weather?q=";
         const metricSystem = "units=metric";
         const message = " , but who cares?";
         let listItems = self.children();
-/*         function getTemperature(city) {
-            const APIKey = "bf03de8120fbabf27fdd083f9d1fddc0";
-            const baseAPICall = "http://api.openweathermap.org/data/2.5/weather?q=";
-            const metricSystem = "units=metric";
-            let url = baseAPICall + city + "&" + metricSystem + "&&APPID=" + APIKey;
-            $.getJSON(url).done(function(data) {
-                temp = data["main"]["temp"].toFixed(0);
-            });
-        } */
+
         function animate(li) {
             $(li).click(function() {
                 $(this).fadeOut(function() {
@@ -45,12 +40,12 @@ $(document).ready(function() {
                 });
             });
         }
+
         for (let i = 0; i < cities.length; i ++) {
             for (let j = 0; j < listItems.length; j ++) {
                 let listItem = listItems[j];
                 let match = listItem.innerText.toLowerCase().includes(cities[i]);
                 if(match) {
-                    // getTemperature(cities[i]);
                     let url = baseAPICall + cities[i] + "&" + metricSystem + "&&APPID=" + APIKey;
                     $.getJSON(url, function(data) {
                         let appendText = " " + data["main"]["temp"].toFixed(0) + " degrees in " + data["name"] + message;
